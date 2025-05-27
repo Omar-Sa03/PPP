386650b9d1505cd4c022a660353f0b739a151dcf
Omar-Sa03 <mohamedomar.sagga@insat.ucar.tn>
2025-05-27 20:42:34 +0100

    fixed networking error

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://172.190.138.238:80', 'http://localhost:3000'], // Allow your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true, // Allow cookies/auth if needed
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
